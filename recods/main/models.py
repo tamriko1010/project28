109051a652a1697fa908f470a63816cf4414c4b6
Тамара <tamriko1010@rambler.ru>
2024-06-27 18:50:57 +0300

    приложение django

---
+++ recods/main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+
